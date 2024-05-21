193c5c88039afd6c33d5f1a249b065262586ca29
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-21 21:24:36 +0900

    Add files via upload

---
+++ Chat_Client/Chat_Client/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Client
+{
+    class Room
+    {
+        public int room_num;
+        public string room_name;
+        public List<string> room_users;
+        public List<Token> current_contact_users;
+        public List<string> room_messages;
+        public Room(int room_num, string room_name)
+        {
+            this.room_num = room_num;
+            this.room_name = room_name;
+            current_contact_users = new List<Token>();
+            room_messages = new List<string>();
+        }
+        public void print_messages()
+        {
+            Console.WriteLine("previous room messages");
+            foreach (var message in room_messages) Console.WriteLine(message);
+        }
+    }
+}
---
+++ Chat_Client/Chat_Client/protocol.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Client
+{
+    public enum PROTOCOL : short
+    {
+        Settings = 0,
+
+        SIGNUP_Request = 1,
+        SIGNUP_Success = 2,
+        SIGNUP_Fail = 3,
+
+        LOGIN_Request = 4,
+        LOGIN_Success = 5,
+        LOGIN_Fail = 6,
+
+        Room_Create_Ready_Request = 7,
+        Room_Create_Ready_Success = 8,
+        Room_Create_Ready_Fail = 9,
+
+        Room_Create_Request = 10,
+        Room_Create_Success = 11,
+        Room_Create_Fail = 12,
+
+        Room_Enter_Request = 13,
+        Room_Enter_Success = 14,
+        Room_Enter_Fail = 15,
+
+        Send_Message = 16,
+        Room_Leave = 17,
+        Room_Leave_Success = 18,
+        Deliver_Message = 19,
+
+        Room_Update = 20
+    }
+}
