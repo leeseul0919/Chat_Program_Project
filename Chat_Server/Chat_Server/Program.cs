ae0a3fe8fc58e004119d6d0ce0b00536d8350c7a
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-22 05:18:55 +0900

    Add files via upload

---
+++ Chat_Server/Chat_Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Chat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkService server = new NetworkService();
+            server.start("192.168.0.175", 7979, 100); //start listen
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
